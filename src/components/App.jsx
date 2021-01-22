e51dcf09d828e48587eaa129adcb3af84cd700b4
Carlos Lopez <krlosaren@gmail.com>
2021-01-22 15:04:23 -0300

    [add] starting basic files

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// styles
+import '../styles/components/app.css'
+
+function App() {
+  return (
+    <div>
+    <h1>
+      Hola Mundo
+    </h1>
+    </div>
+  )
+}
+
+export default App
