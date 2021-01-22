ce95c80a2b931144e3350a091e677a209d724196
Carlos Lopez <krlosaren@gmail.com>
2021-01-22 17:41:44 -0300

    [add] layout component , header and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="footer">
+    <a href="/">
+      <img src="ansilliano.com" alt="" />
+    </a>
+  </div>
+);
+
+export default Footer;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Layout from '../components/Layout';
 // containers
 import { Home, About, Works, NotFound } from '../containers/index';
 
@@ -9,12 +10,14 @@ import '../styles/app.scss';
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/works" component={Works} />
-        <Route component={NotFound} />
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/works" component={Works} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
     </BrowserRouter>
   );
 }
